b6f8e4283c96bcf9871993f47f7cf9effe05c53d
ry0@r002 <tan0ry0atr002@gmail.com>
2019-06-12 02:11:35 +0000

    test hello app

--- hp_calcRy0/urls.py
+++ hp_calcRy0/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import calc.views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+        path('hello/', calc.views.hello),
+        path('admin/', admin.site.urls),
 ]
